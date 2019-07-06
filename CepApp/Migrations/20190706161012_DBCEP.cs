86ea1068e9777caf4c79f9d7a26223459b9928fc
AndreiNalevaiko <nalevaiko72@gmail.com>
2019-07-06 13:53:35 -0300

    Add project files.

---
+++ CepApp/Migrations/20190706161012_DBCEP.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CepApp.Migrations
+{
+    public partial class DBCEP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CEPS",
+                table: "CEPS");
+
+            migrationBuilder.RenameTable(
+                name: "CEPS",
+                newName: "CEP");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CEP",
+                table: "CEP",
+                column: "CepId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CEP",
+                table: "CEP");
+
+            migrationBuilder.RenameTable(
+                name: "CEP",
+                newName: "CEPS");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CEPS",
+                table: "CEPS",
+                column: "CepId");
+        }
+    }
+}
